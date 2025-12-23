e57d8354f081792ff4bc2d9dcae69cb07f7547b3
Steffen <steffen@ravenclaw>
2025-12-23 14:12:55 +0100

    Mosaic: Fix rare off-by-one error

--- games/mosaic.c
+++ games/mosaic.c
@@ -751,14 +751,13 @@ static char *new_game_desc(const game_params *params, random_state *rs,
                     desc[(y*params->width)+x].clue);
                 location_in_str++;
             } else {
-                if (space_count <= 'z') {
-                    space_count++;
-                } else {
+                if (space_count == 'z') {
                     sprintf(compressed_desc + location_in_str, "%c",
                         space_count);
                     location_in_str++;
                     space_count = 'a'-1;
                 }
+                space_count++;
             }
         }
     }
